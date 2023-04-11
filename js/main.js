ec0b404cda8d4971a265e22b94d29a3febbc8ce0
JhengDer <eagle0983850689@gmail.com>
2023-04-11 08:30:09 +0800

    add index and local file

---
+++ js/main.js
@@ -0,0 +1,79 @@
+var swiperCategories = new Swiper(".categories__container", {
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 2,
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 24
+    },
+    1400: {
+      slidesPerView: 7,
+      spaceBetween: 24
+    },
+  },
+});
+var swiperProducts = new Swiper(".new__container", {
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 2,
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    },
+  },
+});
+
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[content]');
+
+// tabs.forEach((tab)=>{
+//   tab.addEventListener('click',()=>{
+//     const target=document.querySelector(tab.dataset.target);
+//     console.log(target);
+//     tabContents.forEach((tabContent)=>{
+//       tabContent.classList.remove('active-tab');
+//     });
+//     target.classList.add('active-tab');
+//     tabs.forEach((tab)=>{
+//       tab.classList.remove('active-tab');
+//     });
+//     tab.classList.add('active-tab');
+//   });
+// });
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('active-tab');
+    });
+    tab.classList.add('active-tab')
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active-tab');
+    });
+    target.classList.add('active-tab');
+  });
+});
